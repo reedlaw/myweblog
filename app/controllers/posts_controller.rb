dd7c164e6a16f103b1ad05383061fe85024ed1a0
reedlaw <reedlaw@gmail.com>
2008-05-22 01:00:37 -0700

    basic blog

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  # GET /posts
+  # GET /posts.xml
+  def index
+    @posts = Post.find(:all)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
+    end
+  end
+
+  # GET /posts/1
+  # GET /posts/1.xml
+  def show
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @post }
+    end
+  end
+end
