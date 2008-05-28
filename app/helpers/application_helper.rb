722b32bf6fde56a84d43560ff574acee6b52adf0
Reed Law <reedlaw@gmail.com>
2008-05-28 10:07:58 +0800

    overode textilize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def textilize(text)
+    if text.blank?
+      ""
+    else
+      textilized = TealCloth.new(text)
+      textilized.to_html
+    end
+  end
+
 end
