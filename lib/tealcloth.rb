36af095eb607da2a2ed95db5c6929db97ccfb598
reedlaw <reedlaw@gmail.com>
2008-05-29 01:40:43 -0700

    style improvement

--- lib/tealcloth.rb
+++ lib/tealcloth.rb
@@ -6,5 +6,9 @@ class TealCloth < RedCloth
   def textile_ruby( tag, atts, cite, content )
     %(<pre><code class="ruby">#{content}</code></pre>)
   end
+  
+  def textile_video( tag, atts, cite, content )
+    %(<div class="video">#{content}</div>)
+  end
 
 end
